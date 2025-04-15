dc18f8b99f471d76458b8d3b55c89d835123b26e
Kira <bergonioxander1@gmail.com>
2025-04-15 08:34:58 +0800

    publicDisplay

--- Activity 1/TestVehicle.java
+++ Activity 1/TestVehicle.java
@@ -6,4 +6,17 @@ class Vehicle {
     private int speed;
     private String fuelType;
 
-   
+    public Vehicle(String brand, int speed, String fuelType) {
+        this.brand = brand;
+        this.speed = speed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Vehicle Brand: " + brand);
+        System.out.println("Speed: " + speed + " km/h");
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
+
